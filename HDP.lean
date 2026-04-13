ffbf31a21808a89c549da1d0a329dae1f7002280
Lean 4 VS Code Extension <>
2026-04-13 00:32:42 -0400

    Initial commit

---
+++ HDP.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `HDP` library.
+-- Import modules here that should be built as part of the library.
+import HDP.Basic
---
+++ Main.lean
@@ -0,0 +1,4 @@
+import HDP
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
